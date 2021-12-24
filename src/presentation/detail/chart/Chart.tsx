6af3626179ae9a2f6b23f6eb8c3639161939e788
Tim Wüllner <tim.wuellner@openknowledge.de>
2021-12-24 12:54:48 +0100

    Update yaxis range in chart

--- src/presentation/detail/chart/Chart.tsx
+++ src/presentation/detail/chart/Chart.tsx
@@ -89,7 +89,6 @@ export const Chart: FunctionComponent<Props> = ({ run }) => {
             stroke={Styles.BACKGROUND_COLOR_SECOND}
             dx={-5}
             style={{ fontSize: Styles.FONT_SIZE_SMALL }}
-            domain={[0, "dataMax"]}
           />
 
           <Area
@@ -111,7 +110,6 @@ export const Chart: FunctionComponent<Props> = ({ run }) => {
             stroke="#ff8c00"
             dx={5}
             style={{ fontSize: Styles.FONT_SIZE_SMALL }}
-            domain={[0, "dataMax"]}
           />
           <Area
             type="monotone"
