058705a1be63ba5be2d80537dff42ff1e639f1d1
Tim Wuellner <tim.wuellner@openknowledge.de>
2023-11-03 18:58:23 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -2,3 +2,8 @@ self.addEventListener("fetch", function (event) {
   // do nothing here, just log all the network requests
   console.log(event.request.url);
 });
+
+self.addEventListener('message', function (evt) {
+  console.log('postMessage received', evt.data);
+  self.registration.showNotification(evt.data)
+})
